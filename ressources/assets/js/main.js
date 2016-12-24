fc2f9b74660d28fa9f117c1aa401188ec8c60898
Adrien Gallou <adriengallou@gmail.com>
2016-12-24 19:19:30 +0100

    adds search the extension site

--- ressources/assets/js/main.js
+++ ressources/assets/js/main.js
@@ -12,3 +12,14 @@ function toggleMenu(element) {
     target.setAttribute("aria-selected", !selected);
     target.setAttribute("aria-hidden", selected);
 }
+
+
+function toggleSearch(isOnFocus)
+{
+    if (isOnFocus) {
+        document.body.classList.add("search-focused");
+    } else {
+        document.body.classList.remove('search-focused');
+    }
+
+}
